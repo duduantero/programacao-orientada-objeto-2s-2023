a4cddc101ac9bbff8aa955ca248745bcd790b361
duduantero <xduduanterox@gmail.com>
2023-08-14 16:20:37 -0300

    att

--- lista1/atividade1/verificaNumeroPrimo.java
+++ lista1/atividade1/verificaNumeroPrimo.java
@@ -10,8 +10,18 @@ public class verificaNumeroPrimo {
         verificarNumeroPrimo(1232132131);
     }
 
-    public static void verificarNumeroPrimo(Integer n){
-
+    /// Função utilizada para verificar apenas se número é primo ou não(Return 1 é primo, Return 0 não é primo )
+    public static int verificarNumeroPrimo(int n) {
+        //Se n tiver valor 1 ou 0, não será 
+        if (n == 0 || n == 1) {
+            return 0;
+        } else {
+            for (int i = 2; i * i <= n; i++) {
+                if (n % i == 0) {
+                    return 0;
+                }
+            }
+            return 1; // The number is prime
+        }
     }
-    
 }
