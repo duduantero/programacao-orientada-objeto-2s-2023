157f7b27d38d8d97615be19f6964734c45d05ff9
duduantero <xduduanterox@gmail.com>
2023-09-18 10:57:49 -0300

    Add lista2 and atividade5

---
+++ lista2/atividade5/Moto.java
@@ -0,0 +1,21 @@
+
+public class Moto extends Veiculo{
+
+    @Override
+    public String toString() {
+        return "Moto []";
+    }
+
+    @Override
+    void acelerar() {
+        // TODO Auto-generated method stub
+        super.acelerar();
+    }
+
+    @Override
+    void frear() {
+        // TODO Auto-generated method stub
+        super.frear();
+    }
+    
+}
---
+++ lista2/atividade5/Veiculo.java
@@ -0,0 +1,13 @@
+
+
+public class Veiculo {
+    protected String marca; 
+    protected String modelo;
+    protected String anoFabricacao;
+
+
+    void acelerar(){}
+    void frear(){}
+
+
+}
